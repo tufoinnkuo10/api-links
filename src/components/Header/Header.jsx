ee18d22e83bab0ba7347a4798124405aa55fd718
Tufoin Nkuo <tufoinnkuo10@gmail.com>
2022-09-16 09:39:32 +0100

    add header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+// import { CallMissedSharp } from "@material-ui/icons";
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
     return(
-        <h1>Header</h1>
+        <AppBar>
+            <Toolbar className={Classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Tufoin Travel
+                </Typography>
+                <Box display="flex">
+                <Typography variant="h6" className={classes.title}>
+                    Tufoin adventures
+                </Typography>
+                <Autocomplete>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputinput}} />
+                    </div>
+                </Autocomplete>
+
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
